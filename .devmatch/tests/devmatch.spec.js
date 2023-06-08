70099dbe720abfa6f43e3031c5d7a7e16e07b106
Alan Gonzalez <alanboy@alanboy.net>
2023-06-07 18:28:22 -0700

    improve build time?

--- .devmatch/tests/devmatch.spec.js
+++ .devmatch/tests/devmatch.spec.js
@@ -8,11 +8,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('SPICYIO_COPYRIGHT', () => {
   // Make sure the old elements are not present
-  test('REMOVE_STATIC_HTML', async ({ page }) => {
-    // await expect(page.locator('.resip_section')) .not.toContainText('Homemade');
-    // await expect(page.locator('.resip_section')) .not.toContainText('Noodles');
-    // await expect(page.locator('.resip_section')) .not.toContainText('Egg');
-    // await expect(page.locator('.resip_section')) .not.toContainText('Sushi Dizzy');
+  test('UPDATE_YEAR', async ({ page }) => {
     await expect(page.locator('.copyright')).not.toContainText('2019');
   });
 
